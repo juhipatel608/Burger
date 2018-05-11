87e753f2b5c36408b8d0dc5e504944a0b1a1e864
Your Name <juhipatel608@gmail.com>
2018-05-10 21:53:07 -0500

    really trying to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
+var connection;
 
 if (process.env.JAWSDB_URL ) {
   connection =mysql.createConnection (process.env.JAWSDB_URL); 
 } else{
 connection =mysql.createConnection({
-  host: "localroot",
+  host: "localhost",
   user: "root",
   password: "",
   database: "burgers_db"
